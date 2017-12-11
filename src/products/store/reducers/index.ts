d68e38b56ca7473b602f6e3d391dd432e322b173
Arne Tesch <arnetesch@Arnes-MacBook-Pro.local>
2017-12-11 19:26:18 +0100

    concept of effects

---
+++ src/products/store/reducers/index.ts
@@ -0,0 +1,45 @@
+import { ProductState } from "./index";
+import {
+  ActionReducerMap,
+  createSelector,
+  createFeatureSelector
+} from "@ngrx/store";
+import * as fromPizzas from "./pizzas.reducer";
+
+export interface ProductState {
+  pizzas: fromPizzas.PizzaState;
+}
+// STATE TREE ==>
+/*
+const state = {
+  products: {
+    pizzas: {
+      data: [],
+      loaded: false,
+      loading: false
+    }
+  }
+};
+*/
+
+export const reducers: ActionReducerMap<ProductState> = {
+  pizzas: fromPizzas.reducer
+};
+
+export const getProductsState = createFeatureSelector<ProductState>("products");
+
+// pizzas state
+export const getPizzaState = createSelector(
+  getProductsState,
+  (state: ProductState) => state.pizzas
+);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+export const getPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
