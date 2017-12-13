f4c3f087aa55080c177d3b605c757190844cfb4e
Arne Tesch <arnetesch@Arnes-MacBook-Pro.local>
2017-12-13 19:38:26 +0100

    Extend state tree

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,10 +1,5 @@
-import { ProductState } from "./index";
-import {
-  ActionReducerMap,
-  createSelector,
-  createFeatureSelector
-} from "@ngrx/store";
-import * as fromPizzas from "./pizzas.reducer";
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
+import * as fromPizzas from './pizzas.reducer';
 
 export interface ProductState {
   pizzas: fromPizzas.PizzaState;
@@ -23,31 +18,7 @@ const state = {
 */
 
 export const reducers: ActionReducerMap<ProductState> = {
-  pizzas: fromPizzas.reducer
+  pizzas: fromPizzas.reducer,
 };
 
-export const getProductsState = createFeatureSelector<ProductState>("products");
-
-// pizzas state
-export const getPizzaState = createSelector(
-  getProductsState,
-  (state: ProductState) => state.pizzas
-);
-
-export const getPizzasEntities = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasEntities
-);
-
-export const getAllPizzas = createSelector(getPizzasEntities, entities => {
-  return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
-});
-
-export const getPizzasLoaded = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoaded
-);
-export const getPizzasLoading = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoading
-);
+export const getProductsState = createFeatureSelector<ProductState>('products');
